2b12b686336548f6b65b1d52804ab4b58f259e23
snak1o <snakwcs@gmail.com>
2022-07-12 16:02:59 +0300

    Add dashboard layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="text-3xl">Информация</h1>
+    <div class="grid grid-cols-4 gap-10 pt-10">
+      <StatsCard :class="classList[item - 1]" v-for="item in 4" :key="item">
+        <template #count>1000</template>
+        <template #title>Пользователи</template>
+      </StatsCard>
+    </div>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StatsCard from "@/components/StatsCard";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      classList: ['bg-yellow-500', 'bg-purple-400', 'bg-green-600', 'bg-indigo-400'],
+    }
+  },
+  components: {StatsCard},
+
 }
 </script>
