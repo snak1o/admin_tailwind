54c38c784a8c5a15e0a5205800dd56d606537462
snak1o <snakwcs@gmail.com>
2022-07-12 16:09:57 +0300

    Add categories pages

---
+++ src/views/Categories/All.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-3xl">Категории</h1>
+</template>
+
+<script>
+export default {
+  name: "Categories"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Categories/_category.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-2xl">{{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  name: "_category"
+}
+</script>
+
+<style scoped>
+
+</style>
