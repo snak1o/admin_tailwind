2664c14617a11de174e48d4a84694653d84fa467
snak1o <snakwcs@gmail.com>
2022-07-12 16:08:55 +0300

    Add sidebar and statistics card components layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,70 @@
+<template>
+  <aside class="w-1/6 h-screen fixed">
+    <div class="overflow-y-auto h-full py-4 px-3 bg-gray-50 rounded-r" >
+      <span class="flex text-2xl text-gray-900 font-normal pb-5 justify-center">r-Batskydd</span>
+      <ul class="space-y-2">
+        <li>
+          <router-link to="/" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+            </svg>
+            <span class="ml-3">Информация</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/categories" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Категории</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/products" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" />
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Товары</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/orders" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" />
+              <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" />
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Заказы</span>
+            <span class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full">3</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/colors" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z" clip-rule="evenodd" />
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Цвета</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/users" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Пользователи</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StatsCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4 rounded-2xl">
+    <h2 class="title-font font-medium sm:text-4xl text-3xl text-white"><slot name="count"/></h2>
+    <p class="leading-relaxed text-white"><slot name="title"/></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatsCard"
+}
+</script>
+
+<style scoped>
+
+</style>
