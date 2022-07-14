fd61eafe8d40ce321ef69606ee11865afcdcbd77
snak1o <snakwcs@gmail.com>
2022-07-14 17:30:26 +0300

    Create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="show" class="w-screen h-screen bg-zinc-900/50 z-10 absolute top-0 left-0 flex justify-center items-center" @click="$emit('close')">
+    <div @click.stop class="flex bg-white rounded-md border p-5">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
