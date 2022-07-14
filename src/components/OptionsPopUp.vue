1e34b94d252e643b23c67553fc1f5bd373347e78
snak1o <snakwcs@gmail.com>
2022-07-14 21:47:18 +0300

    Add options popup component

---
+++ src/components/OptionsPopUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="show" class="absolute bg-white rounded text-black z-10" @click="handleOptions" @mouseleave="show = false">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OptionsPopUp",
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    handleOptions() {
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
