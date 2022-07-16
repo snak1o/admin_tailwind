823590e1ff17ab0e02bc31c64b7ee40be0bfeca3
snak1o <snakwcs@gmail.com>
2022-07-16 22:37:47 +0300

    Add 24h field stats

--- src/components/StatsCard.vue
+++ src/components/StatsCard.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="p-4 rounded-2xl">
-    <h2 class="title-font font-medium sm:text-4xl text-3xl text-white"><slot name="count"/></h2>
-    <p class="leading-relaxed text-white"><slot name="title"/></p>
+  <div class="flex justify-between p-4 rounded-2xl">
+    <div>
+      <h2 class="title-font font-medium sm:text-4xl text-3xl text-white">{{count}}</h2>
+      <p class="leading-relaxed text-white">{{title}}</p>
+    </div>
+    <div>
+      <h2 class="title-font font-medium text-white">24ч. +{{dayCount}}</h2>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "StatsCard"
+  name: "StatsCard",
+  props: ['title', 'count', 'dayCount']
 }
 </script>
 
