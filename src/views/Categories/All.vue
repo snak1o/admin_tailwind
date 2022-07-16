831f1ec54f2ab4bc6746ff2d7ff45899d98ef817
snak1o <snakwcs@gmail.com>
2022-07-16 22:40:41 +0300

    Add exception layout for empty category list

--- src/views/Categories/All.vue
+++ src/views/Categories/All.vue
@@ -1,5 +1,11 @@
 <template>
+  <div class="flex h-full flex-col">
   <h1 class="text-3xl">Категории</h1>
+  <div class="flex flex-col items-center h-full justify-center">
+    <span class="text-xl">Нет категорий для показа</span>
+    <button class="px-8 py-2 mt-7 bg-green-700 rounded-md text-white" @click="$router.push('/categories/create')">Добавить</button>
+  </div>
+  </div>
 </template>
 
 <script>
