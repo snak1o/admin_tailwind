03b1b302b9188ae08c9a37172c4928dbf8e9772c
snak1o <snakwcs@gmail.com>
2022-07-17 22:52:47 +0300

    Code refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="text-3xl">Информация</h1>
     <div class="grid grid-cols-4 gap-10 pt-10">
-      <StatsCard :class="classList[item - 1]" v-for="item in 4" :key="item" title="Пользователи" count="1000" day-count="100"/>
+      <StatsCard class="bg-yellow-500" title="Пользователи" count="1000" day-count="100"/>
+      <StatsCard class="bg-purple-400" title="Пользователи" count="1000" day-count="100"/>
+      <StatsCard class="bg-green-600" title="Пользователи" count="1000" day-count="100"/>
+      <StatsCard class="bg-indigo-400" title="Пользователи" count="1000" day-count="100"/>
+
     </div>
   </div>
 
@@ -13,11 +17,6 @@ import StatsCard from "@/components/StatsCard";
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      classList: ['bg-yellow-500', 'bg-purple-400', 'bg-green-600', 'bg-indigo-400'],
-    }
-  },
   components: {StatsCard},
 
 }
