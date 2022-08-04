0b984c6189f2171f98c21ea653c779997cf80da5
snak1o <snakwcs@gmail.com>
2022-08-04 11:24:39 +0300

    Add product edit logic and validation

--- src/views/Products/_product.vue
+++ src/views/Products/_product.vue
@@ -14,16 +14,21 @@
         </div>
         <div class="flex flex-col mb-2">
           <label class="text-gray-600 mb-1" for="colors">Цвета</label>
-          <ColorSelection id="colors" :color-selected="selectedColor" @selectColor="selectColor" @removeColor="removeColor"/>
+          <ColorSelection id="colors" :color-selected="selectedColors" @selectColor="selectColor" @removeColor="removeColor"/>
         </div>
-
+        <div class="flex flex-col mb-2">
+          <label class="text-gray-600 mb-1" for="category">Категория</label>
+          <select name="category" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none" v-model="selectedCategory">
+            <option v-for="category in categories" :value="category.id" :key="category.id">{{category.name}}</option>
+          </select>
+        </div>
         <div class="flex flex-col mb-2">
           <label class="text-gray-600 mb-1" for="tags">Теги товара</label>
           <input id="tags" v-model="currentTag" @keydown.enter="addTag" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none" required/>
-          <ul v-if="product.tags.length > 0" class="flex mt-2">
-            <li v-for="(tag, index) in product.tags" :key="tag" class="mr-2 bg-blue-500 rounded px-5 text-white relative group">
-              {{tag.name}}
-              <svg @click="product.tags.splice(index, 1)" xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 absolute right-0.5 top-1.5 cursor-pointer opacity-0 transition-opacity group-hover:opacity-100" viewBox="0 0 20 20" fill="currentColor">
+          <ul v-if="tags" class="flex mt-2">
+            <li v-for="(tag, index) in tags" :key="tag" class="mr-2 bg-blue-500 rounded px-5 text-white relative group">
+              {{tag}}
+              <svg @click="tags.splice(index, 1)" xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 absolute right-0.5 top-1.5 cursor-pointer opacity-0 transition-opacity group-hover:opacity-100" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
               </svg>
             </li>
@@ -34,116 +39,148 @@
           <label class="text-gray-600 mb-1" for="sku">Номер товара (SKU)</label>
           <input type="text" id="sku" v-model="product.sku" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-40 p-2.5 outline-none" required/>
         </div>
-
-        <div class="flex flex-col mb-2">
-          <label class="text-gray-600 mb-1" for="price">Цена товара без ндс (€)</label>
-          <input type="number" min="1" v-model="product.price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-40 p-2.5 outline-none" required/>
-        </div>
         <div class="flex flex-col mb-2">
           <label class="text-gray-600 mb-1" for="pricewithalv">Цена товара включая ндс (€)</label>
-          <input type="number" min="1" :value="product.pricealv = (product.price + product.price * 0.24).toFixed(2)" id="pricewithalv" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg cursor-not-allowed focus:ring-blue-500 focus:border-blue-500 block w-40 p-2.5 outline-none" disabled/>
+          <input type="number" min="1" v-model="priceAlv" id="pricewithalv" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-40 p-2.5 outline-none" required/>
         </div>
         <UploadFile />
         <button @click="updateProduct()" class="px-8 py-2 mt-2 bg-green-700 rounded-md text-white">Сохранить</button>
       </div>
       <div class="flex flex-col w-3/6 ml-5">
         <label class="text-gray-600 mb-1" for="photo">Фотографии</label>
-        <PhotoPreview :selected="product.photos" id="photo" @add="selectPhoto" @remove="removePhoto"/>
+        <PhotoPreview :selected="selectedPhotos" id="photo" @add="selectPhoto" @remove="removePhoto"/>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-// import axios from "axios";
-
 import UploadFile from "@/components/UploadFile";
 import PhotoPreview from "@/components/PhotoPreview";
 import ColorSelection from "@/components/ColorSelection";
+import axios from "axios";
+import store from "@/store";
+
 export default {
   name: "_product",
   components: {ColorSelection, PhotoPreview, UploadFile},
   data() {
     return {
       currentTag: "",
-      selectedColor: [],
-      product: {
-        "id": 1,
-        "name": "новая лодочка",
-        "description": "описание для новой лодочки",
-        "categoryId": 1,
-        "tags": [
-          {
-            "name": "tag1"
-          },
-          {
-            "name": "tag2"
-          }
-        ],
-        "photos": ["https://sun9-87.userapi.com/impg/c855724/v855724637/250c75/9HcuEIaG8kI.jpg?size=783x744&quality=96&sign=93a5e5bed4fba2b03e2ea4f1bc4276d8&type=album",
-          "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-        "colors": [
-          {
-            "name": "Dark Gray",
-            "hex": "#4a4848",
-            "id": 1
-          },
-          {
-            "name": "Red",
-            "hex": "#ff0000",
-            "id": 3
-          }
-        ],
-        "price": 32.26,
-        "pricealv": 40,
-        "sku": "F23"
-      }
+      selectedColors: [],
+      selectedPhotos: [],
+      product: [],
+      priceAlv: 0,
+      tags: [],
+      categories: [],
+      selectedCategory: null,
     }
   },
   async mounted() {
-    // const res = await axios.get('/api/product/byId/' + this.$route.params.id)
-    // this.product = res.data
+    const product = await axios.get(process.env.VUE_APP_API + '/api/v1/items/' + this.$route.params.id)
+    this.product = product.data
+
+    const categories = await axios.get(process.env.VUE_APP_API + '/api/v1/categories/')
+    this.categories = categories.data
+
     for (let i in this.product.colors) {
-      this.selectedColor.push(this.product.colors[i].id)
+      this.selectedColors.push(this.product.colors[i].id)
     }
+    for (let i in this.product.images) {
+      this.selectedPhotos.push(this.product.images[i].id)
+    }
+    this.priceAlv = this.product.price * 1.24
+
+    if (this.product.tags) {
+      for (let i in this.product.tags) {
+        this.tags.push(this.product.tags[i].name)
+      }
+    }
+    this.selectedCategory = this.product.category.id
   },
   methods: {
     addTag(){
       if (this.currentTag.trim() !== '') {
-        this.product.tags.push({"name": this.currentTag})
+        this.tags.push(this.currentTag)
         this.currentTag = ""
       }
     },
-    updateProduct() {
-      console.log('updated')
+    async updateProduct() {
+      if (this.validateProduct()) {
+        let price = parseFloat((this.priceAlv - (this.priceAlv * 0.1935)).toFixed(2))
+        try {
+          const res = await axios.put(process.env.VUE_APP_API + '/api/v1/items/' + this.product.id, {
+            categoryId: this.selectedCategory,
+            colors: this.selectedColors,
+            description: this.product.description,
+            images: this.selectedPhotos,
+            name: this.product.name,
+            price: price,
+            sku: this.product.sku,
+            tags: this.tags
+          })
+          if (res.status === 200) {
+            await store.dispatch('addNotification', `Продукт #${this.product.id} обновлен успешно.`)
+          }
+        } catch (err) {
+          await store.dispatch('addNotification', `Ошибка ${err.response.status}.`)
+        }
+      }
     },
     selectPhoto(value) {
-      this.product.photos.push(value)
+      this.selectedPhotos.push(value)
     },
     removePhoto(value) {
-      let index = this.product.photos.indexOf(value)
+      let index = this.selectedPhotos.indexOf(value)
       if (index >= 0) {
-        this.product.photos.splice(index, 1)
+        this.selectedPhotos.splice(index, 1)
       }
     },
     selectColor(value) {
-      this.selectedColor.push(value.id)
-      this.product.colors.push(value)
+      console.log(value)
+      this.selectedColors.push(value)
     },
     removeColor(value) {
-      let selectedIndex = this.selectedColor.indexOf(value.id)
-      let index
-      for (let i in this.product.colors) {
-        if (this.product.colors[i].id === value.id) {
-          index = i
+      for (let i in this.selectedColors) {
+        if (this.selectedColors[i] === value){
+          this.selectedColors.splice(parseInt(i), 1)
         }
       }
-      if (selectedIndex >= 0 && index >= 0) {
-        this.selectedColor.splice(selectedIndex, 1)
-        this.product.colors.splice(index, 1)
+    },
+    validateProduct() {
+      if (this.product.name.trim() === "") {
+        store.dispatch('addNotification', "Название продукта не может быть пустым.")
+        return false
       }
-    }
-  }
+      if (this.product.description.trim() === "") {
+        store.dispatch('addNotification', "Описание продукта не может быть пустым.")
+        return false
+      }
+      if (this.selectedColors.length === 0) {
+        store.dispatch('addNotification', "Выберите хотя бы один цвет.")
+        return false
+      }
+      if (this.selectedPhotos.length === 0) {
+        store.dispatch('addNotification', "Выберите хотя бы одну фотографию.")
+        return false
+      }
+      if (this.selectedCategory === null) {
+        store.dispatch('addNotification', "Выберите категорию продукта.")
+        return false
+      }
+      if (this.product.sku === "") {
+        store.dispatch('addNotification', "Уникальный номер товара не может быть пустым.")
+        return false
+      }
+      if (typeof(this.priceAlv) !== 'number' || this.priceAlv <= 0) {
+        store.dispatch('addNotification', "Цена должна быть больше нуля.")
+        return false
+      }
+      else {
+        return true
+      }
+    },
+  },
 }
 </script>
 
