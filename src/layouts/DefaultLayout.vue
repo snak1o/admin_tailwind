6a93209b41f166a164b6b93c7d132d9f32ffdc95
snak1o <snakwcs@gmail.com>
2022-08-04 11:21:48 +0300

    Add notification component to layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,18 @@
 <template>
   <Sidebar/>
   <router-view class="w-5/6 flex-1 float-right p-5 h-screen"/>
+  <Notification />
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar";
+import Notification from "@/components/Notification";
 
 export default {
   name: "DefaultLayout",
   components: {
     Sidebar,
+    Notification
   }
 }
 </script>
