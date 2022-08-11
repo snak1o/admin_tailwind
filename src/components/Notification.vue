04a17fbd779e85b00b9d9241e729396a2f1b466a
snak1o <snakwcs@gmail.com>
2022-08-12 02:24:12 +0300

    Add notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,31 @@
+<template>
+    <TransitionGroup name="list" tag="div" class="fixed bottom-3 left-3 space-y-2">
+      <div class="bg-indigo-500 text-white p-3 rounded-md w-96 break-words" v-for="item in notifications" :key="item">
+        {{item}}
+      </div>
+    </TransitionGroup>
+
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Notification",
+  computed: {
+    ...mapGetters(['notifications'])
+  }
+}
+</script>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(-60px);
+}
+</style>
