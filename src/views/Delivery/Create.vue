5bcda6eab27a55167ab3bfbc2faad46c2092ea3c
snak1o <snakwcs@gmail.com>
2022-08-12 02:22:27 +0300

    Add delivery create logic

---
+++ src/views/Delivery/Create.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <button @click="createDelivery">ff</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Create",
+  data() {
+    return {
+      companyName: '4444334',
+      deliveryName: '4324242344',
+      price: 100,
+    }
+  },
+  methods: {
+    async createDelivery() {
+      const res = await this.$axios.post('/api/v1/delivery/create', {
+        companyName: this.companyName,
+        name: this.deliveryName,
+        price: this.price
+      })
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
