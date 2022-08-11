66388324270584ebf0401e3b1dd87287c55dbc2c
snak1o <snakwcs@gmail.com>
2022-08-12 02:14:28 +0300

    Add authorization validation on app mount

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <script>
 import DefaultLayout from "@/layouts/DefaultLayout";
 import EmptyLayout from '@/layouts/EmptyLayout';
+import store from "@/store";
 
 export default {
   components: {
@@ -14,6 +15,16 @@ export default {
   computed: {
     layout() {
       return this.$store.getters.layout
+    }
+  },
+  async mounted() {
+    if (localStorage.getItem('token') && localStorage.getItem('token').length > 0) {
+        const res = await this.$axios.get('/api/v1/users/me')
+
+        if (res && res.status === 200) {
+          await store.commit('setLoggedIn', true)
+        }
+      console.log(res)
     }
   }
 }
