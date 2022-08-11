bd20c36eae7f24a036bf9cf68a34ffde2e402bf4
snak1o <snakwcs@gmail.com>
2022-08-12 02:09:18 +0300

    Change app login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div class="h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Панель администратора</h2>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <div class="mt-8 space-y-6">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Логин</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Логин">
+            <input id="email-address" name="email" type="text" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" v-model="login" placeholder="Логин">
           </div>
           <div>
             <label for="password" class="sr-only">Пароль</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Пароль">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" v-model="password" placeholder="Пароль">
           </div>
         </div>
 
-        <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <div @click="logIn">
+          <button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <span class="absolute left-0 inset-y-0 flex items-center pl-3">
             <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
@@ -28,17 +28,51 @@
             Вход
           </button>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
+import store from "@/store";
+
 export default {
   name: "Login",
   mounted() {
     this.$store.commit('setLayout', 'EmptyLayout')
-  }
+  },
+  data() {
+    return {
+      login: '',
+      password: '',
+    }
+  },
+  methods: {
+    async logIn() {
+      try {
+        const res = await axios.post('/api/v1/users/sign-in', {
+          login: this.login,
+          password: this.password
+        })
+        if (res && res.status === 200) {
+          const user = await this.$axios.get('/api/v1/users/me')
+          if (user && user.status === 200 && user.data.admin) {
+            await localStorage.setItem('token', res.data.accessToken)
+            await store.commit('setLoggedIn', true)
+            this.$router.push('/')
+          }
+          else {
+            localStorage.removeItem('token')
+            return false
+          }
+        }
+      }catch (e) {
+        console.log(e)
+      }
+    }
+  },
 }
 </script>
 
