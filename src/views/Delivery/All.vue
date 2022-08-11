d84ea3ea7c3206d1f35737f95239841a345ece1a
snak1o <snakwcs@gmail.com>
2022-08-12 02:21:18 +0300

    Add delivery list logic

---
+++ src/views/Delivery/All.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    {{delivery}}
+    <button @click="deleteDelivery">11</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "All",
+  async mounted() {
+      const res = await this.$axios.get('/api/v1/delivery/all')
+      console.log(res)
+      this.delivery = res.data
+  },
+  data() {
+    return {
+      delivery: [],
+    }
+  },
+  methods: {
+    async deleteDelivery() {
+      await this.$axios.delete('/api/v1/delivery/1')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
