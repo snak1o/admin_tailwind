c8c8cb6c744976d5bd68392d7eab23f6cb57f6b7
snak1o <snakwcs@gmail.com>
2022-08-12 02:20:25 +0300

    Add delivery edit logic

---
+++ src/views/Delivery/_delivery.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    {{delivery}}
+    <button @click="updateDelivery">1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "_delivery",
+  data() {
+    return {
+      delivery: {},
+    }
+  },
+  async mounted() {
+    const res = await this.$axios.get('/api/v1/delivery/' + this.$route.params.id)
+    this.delivery = res.data
+  },
+  methods: {
+    async updateDelivery() {
+      this.delivery.price = 10
+      await this.$axios.put('/api/v1/delivery/' + this.delivery.id, {
+        companyName: this.delivery.companyName,
+        name: this.delivery.name,
+        price: this.delivery.price
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
