4265d0e2d9ce36e260d184b41bad208863bba4d1
snak1o <snakwcs@gmail.com>
2022-08-12 02:05:53 +0300

    Add category create logic and layout

--- src/views/Categories/_category.vue
+++ src/views/Categories/_category.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1 class="text-2xl">{{$route.params.id}}</h1>
+  <section class="pb-5 space-y-2">
+    <h1 class="text-2xl mb-5">Создание категории</h1>
+    <div class="flex flex-col w-1/3">
+      <label class="text-gray-600 mb-1" for="name">Заголовок</label>
+      <input type="text" min="1" id="name" v-model="category.name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 outline-none" required/>
+    </div>
+    <div class="flex flex-col space-y-2 w-1/3">
+      <button class="px-8 py-2 bg-green-700 rounded-md text-white" @click="updateCategory">Обновить категорию</button>
+      <button class="px-8 py-2 bg-red-700 rounded-md text-white" @click="deleteCategory">Удалить категорию</button>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "_category"
+  name: "_category",
+  async mounted() {
+    const {data} = await this.$axios.get('/api/v1/categories/' + this.$route.params.id)
+    this.category = data
+  },
+  data() {
+    return {
+      category: {}
+    }
+  },
+  methods: {
+    async updateCategory() {
+      const res = await this.$axios.put('/api/v1/categories/' + this.category.id, {
+        name: this.category.name
+      })
+      console.log(res)
+    },
+    async deleteCategory() {
+      const res = await this.$axios.delete('/api/v1/categories/' + this.category.id)
+      await this.$router.push('/caregories')
+      console.log(res)
+    }
+  }
 }
 </script>
 
