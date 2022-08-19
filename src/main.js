c2043d1ada039e5520201fd1bdb775decc7ac5e9
snak1o <snakwcs@gmail.com>
2022-08-19 14:04:51 +0300

    Change axios plugin path Set axios baseurl

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
-import instance from "../plugins/axios";
+import instance from "@/plugins/axios";
+import axios from "axios";
 
 const app = createApp(App)
 
 app.config.globalProperties.$axios = {...instance}
+
+axios.defaults.baseURL = process.env.VUE_APP_API
 
 app.use(store)
 
