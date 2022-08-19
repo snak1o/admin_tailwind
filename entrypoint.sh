4617f17c0be59bc2a87339396e0f4629f9db3427
Arteandr <artedamatik@gmail.com>
2022-08-19 17:24:53 +0300

    Update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Replacing env constants"
 for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html;
 do
 	echo "Proccessing $file ...";
-	sed -i 's|VUE_API_URL|'${VUE_API_URL}'|g' $file
+	sed -i 's|VUE_API_URL|'${HOST_API}'|g' $file
 done
 
 echo "Replacing done"
