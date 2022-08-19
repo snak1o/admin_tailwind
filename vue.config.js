ff09c628f3847f9938d603040067504fb93db2af
snak1o <snakwcs@gmail.com>
2022-08-19 14:05:02 +0300

    Remove dev server

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,2 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            "^/api": {
-                target: process.env.VUE_APP_API,
-                changeOrigin: true,
-                ws: true
-            }
-        }
-    }
 }
