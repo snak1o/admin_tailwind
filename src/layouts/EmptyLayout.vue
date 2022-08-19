d1239037eef09fbaf5ff1766c59d7917abf22aee
snak1o <snakwcs@gmail.com>
2022-08-19 14:03:15 +0300

    Add notification showing on empty layout

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -1,9 +1,15 @@
 <template>
   <router-view/>
+  <Notification />
 </template>
 
 <script>
+import Notification from "@/components/Notification";
+
 export default {
   name: "EmptyLayout",
+  components: {
+    Notification
+  }
 }
 </script>
