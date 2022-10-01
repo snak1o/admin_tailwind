d5ec9da00bcd4f30f5f1787dcad66880ac4058e9
snak1o <snakwcs@gmail.com>
2022-10-01 13:15:13 +0300

    Add two digits after the decimal point

--- src/views/Delivery/All.vue
+++ src/views/Delivery/All.vue
@@ -14,7 +14,7 @@
           <span class="text-xl">{{item.name}}</span>
           <span>{{item.companyName}}</span>
         </div>
-        <div class="h-full text-xl flex items-center">{{item.price}}€</div>
+        <div class="h-full text-xl flex items-center">{{item.price.toFixed(2)}}€</div>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 absolute top-2 right-2 opacity-0 group-hover:opacity-100" viewBox="0 0 20 20" fill="currentColor" @click.stop="deleteDelivery(item.id)">
           <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
