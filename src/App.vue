295d0146871fb4357fbdb4847262fb869efb595e
snak1o <snakwcs@gmail.com>
2022-10-01 13:17:43 +0300

    Add loading component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <component v-bind:is="layout"></component>
+  <div v-if="loading" class="flex justify-center items-center h-screen">
+    <div class="border-4 border-gray-400 border-t border-t-indigo-500 w-20 h-20 animate-spin rounded-full border-t-4"></div>
+  </div>
+  <component v-else v-bind:is="layout"></component>
 </template>
 
 <script>
@@ -14,6 +17,9 @@ export default {
   computed: {
     layout() {
       return this.$store.getters.layout
+    },
+    loading() {
+      return (this.$route.path !== '/login' && !this.$store.getters.loggedIn);
     }
   },
 }
